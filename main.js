bcb9d1060a9f28a4f3fff8eca78f69f3c5d8b80e
Kevin Harrison <kharrison7@elon.edu>
2017-08-01 11:36:27 -0400

    This actually uses the DOM.

--- main.js
+++ main.js
@@ -15,62 +15,33 @@ let tiles_Flipped = 0;
 
 
 
+// Code to make tiles appear.
+// This makes the itemList node attach to the total_board element on the HTML page.
+let itemList = document.querySelector( "#total_board" );
+
+
+
+// This makes a loop to go through the memor_Options array.
+for( let i = 0; i < memory_Options.length; i++ ){
+
+// This creates an li value to act as a tile.
+    let li       = document.createElement( "li" );
+
+// This creates an H2 element.
+    let liH2     = document.createElement( "h2" );
+
+// This places the memor_Options text into the liText.
+    let liText = document.createTextNode( memory_Options[ i ] );
+
+// This moves the text to the H2.
+    liH2.appendChild( liText );
+// This adds the H2 to the li tile.
+    li.appendChild( liH2 );
+// This adds the li to the itemList.
+    itemList.appendChild( li );
+}
 
 
 
 
-
-
-// Declare variables.
-// var randomizedWords = [],
-//   pickedWords = [],
-//   preventDups = [],
-//   filtered = [],
-//   matchedCount = 0;
-//
-//
-// // Generate each square and randomized words pattern.
-// for(let i = 18; i > 0; i--) {
-//   $('#total_board').append('<div class="tile" id="' + i +
-//   '" onclick="flipHere(this)"></div>');
-//   let randomWord = memory_Options[Math.floor(Math.random() * (memory_Options.length -1))];
-//   if(randomizedWords.indexOf(randomWord) == -1) {
-//     randomizedWords.push(randomWord);
-//   }
-// }
-
-
-// This makes a tile in the board.
-// function fillWithTiles{
-//
-//   let total_board = document.getElementById( "total_board" );
-//   let heroTitle = document.createElement( "div" );
-//   let heroTitleContent = document.createTextNode( "Tile Fill" );
-//   heroTitle.appendChild( heroTitleContent );
-//   total_board.appendChild( heroTitle );
-// }
-
-
-// This creates a shuffle method for Array Objects:
-// Array.protoype.memory_tile_shuffle = function(){
-// let i = item.length, b, holdValue;
-// while(--i > 0){
-//   b = Math.floor(Math.random() * (i+1));
-//   holdValue = item[b];
-//   item[b] = item[i];
-//   item[i] = holdValue;
-//  }
-// }
-
-
-// function generateBoard(){
-// tiles_Flipped = 0;
-// let output = '';
-// memory_Options.memory_tile_shuffle();
-// for(let i = 0; i < memory_Options.length; i++){
-//   //  output += '<div id="title_'+i+''"onclick= "memoryFlipTile(item,\''+memory_Options[i]+'/')" ></div>';
-//
-//   document.getElementById('total_board').innerHTML = output;
-//
-//  }
-// }
+// This generates a random order of memory_Options Array.
