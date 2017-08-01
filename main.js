d324a73571b2e1a7e7b1696da6e721fdc324d9f2
Kevin Harrison <kharrison7@elon.edu>
2017-07-31 22:13:35 -0400

    This put in black outlines for tiles.

--- main.js
+++ main.js
@@ -13,25 +13,64 @@ let memory_TileIDs = [];
 // Keeps track of the number of tiles flipped.
 let tiles_Flipped = 0;
 
+
+
+
+
+
+
+
+
+// Declare variables.
+// var randomizedWords = [],
+//   pickedWords = [],
+//   preventDups = [],
+//   filtered = [],
+//   matchedCount = 0;
+//
+//
+// // Generate each square and randomized words pattern.
+// for(let i = 18; i > 0; i--) {
+//   $('#total_board').append('<div class="tile" id="' + i +
+//   '" onclick="flipHere(this)"></div>');
+//   let randomWord = memory_Options[Math.floor(Math.random() * (memory_Options.length -1))];
+//   if(randomizedWords.indexOf(randomWord) == -1) {
+//     randomizedWords.push(randomWord);
+//   }
+// }
+
+
+// This makes a tile in the board.
+// function fillWithTiles{
+//
+//   let total_board = document.getElementById( "total_board" );
+//   let heroTitle = document.createElement( "div" );
+//   let heroTitleContent = document.createTextNode( "Tile Fill" );
+//   heroTitle.appendChild( heroTitleContent );
+//   total_board.appendChild( heroTitle );
+// }
+
+
 // This creates a shuffle method for Array Objects:
-Array.protoype.memory_tile_shuffle = function(){
-let i = item.length, b, holdValue;
-while(--i > 0){
-  b = Math.floor(Math.random() * (i+1));
-  holdValue = item[b];
-  item[b] = item[i];
-  item[i] = holdValue;
- }
-}
+// Array.protoype.memory_tile_shuffle = function(){
+// let i = item.length, b, holdValue;
+// while(--i > 0){
+//   b = Math.floor(Math.random() * (i+1));
+//   holdValue = item[b];
+//   item[b] = item[i];
+//   item[i] = holdValue;
+//  }
+// }
 
-function generateBoard(){
-tiles_Flipped = 0;
-let output = '';
-memory_Options.memory_tile_shuffle();
-for(let i = 0; i < memory_Options.length; i++){
-  //  output += '<div id="title_'+i+''"onclick= "memoryFlipTile(item,\''+memory_Options[i]+'/')" ></div>';
 
-  document.getElementById('total_board').innerHTML = output;
-
- }
-}
+// function generateBoard(){
+// tiles_Flipped = 0;
+// let output = '';
+// memory_Options.memory_tile_shuffle();
+// for(let i = 0; i < memory_Options.length; i++){
+//   //  output += '<div id="title_'+i+''"onclick= "memoryFlipTile(item,\''+memory_Options[i]+'/')" ></div>';
+//
+//   document.getElementById('total_board').innerHTML = output;
+//
+//  }
+// }
