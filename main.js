60c8988d6e013e454d9c8839d2c036fed390083b
Kevin Harrison <kharrison7@elon.edu>
2017-08-02 09:22:33 -0400

    This adds a basic clock.

--- main.js
+++ main.js
@@ -27,9 +27,9 @@ function shuffle(){
     // This gives a random integer within the memory_Options.length values.
     // console.log("trying to pop");
     let r = Math.floor((Math.random() * hold_Array.length));
-    console.log("Iteration:",i,"of",memory_Options.length,"possibilities");
-    console.log("memory options:",memory_Options);
-    console.log("r:",r,"of",hold_Array.length,"possibilities");
+    // console.log("Iteration:",i,"of",memory_Options.length,"possibilities");
+    // console.log("memory options:",memory_Options);
+    // console.log("r:",r,"of",hold_Array.length,"possibilities");
     random_Set[i] = hold_Array[r];
     hold_Array.splice(r,1);
     // random_Set[i] = hold_Array.pop(r);
@@ -41,6 +41,15 @@ function shuffle(){
 console.log(random_Set);
 return random_Set;
 }
+
+
+
+
+function clickToFlip(tile_num){
+console.log("Clicked a tile");
+
+}
+
 
 
 
@@ -63,6 +72,9 @@ let itemList = document.querySelector( "#total_board" );
 // This places the memor_Options text into the liText.
     let liText = document.createTextNode( random_Set[ i ] );
 
+// This makes the tile respond to being clicked.
+    li.setAttribute("onclick", "clickToFlip()");
+
 // This moves the text to the H2.
     liH2.appendChild( liText );
 // This adds the H2 to the li tile.
@@ -72,5 +84,50 @@ let itemList = document.querySelector( "#total_board" );
   }
 }
 
+// This places the header and the clock.
+function placeHeader(){
+  let itemList = document.querySelector( "#header" );
 
+  let li       = document.createElement( "div" );
+// This creates an H2 element.
+  let liH2     = document.createElement( "h2" );
+// This places the memor_Options text into the liText.
+  let liText = document.createTextNode( "Memory Matching" );
+// This moves the text to the H2.
+  liH2.appendChild( liText );
+// This adds the H2 to the li tile.
+  li.appendChild( liH2 );
+// This adds the li to the itemList.
+  itemList.appendChild( li );
+}
+
+// This makes the clock tick.
+
+var counter = 0;//set this to what ever you want the start # to be
+function tickTock(){
+		counter++;//increment the counter by 1
+		setTimeout ( "tickTock()", 1000 );//runs itsself after 1000 miliseconds
+		//console.log(counter);uncomment to can see it in action, only with firebug
+		// console.log(counter + "seconds.")
+    let liText = counter;
+
+// This moves the text to the H2.
+    liH2.innerHTML = liText ;
+// This adds the H2 to the li tile.
+    clock.appendChild( liH2 );
+// This adds the li to the itemList.
+    clockSpot.appendChild( clock );
+}
+let clockSpot = document.querySelector( "#clockGoesHere" );
+let clock       = document.createElement( "div" );
+let liH2     = document.createElement( "h2" );
+tickTock();
+
+
+
+
+
+
+// These functions make the game appear.
+placeHeader();
 placeTiles();
