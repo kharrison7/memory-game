ed4f31d61b5875582b9b0ac2ea5e238033c478cb
Kevin Harrison <kharrison7@elon.edu>
2017-08-03 00:30:19 -0400

    This makes some tiles return to hidden.

--- main.js
+++ main.js
@@ -5,8 +5,7 @@ let memory_Options = ['a', 'a', 'b', 'b', 'c', 'c', 'd', 'd', 'e', 'e', 'f', 'f'
 // Holds tile IDs for a given game.
 let memory_Values = [];
 let memory_TileIDs = [];
-// Keeps track of the number of tiles flipped.
-let tiles_Flipped = 0;
+
 
 
 
@@ -33,38 +32,111 @@ function shuffle(){
     // console.log("Random array:",random_Set);
     // console.log("Hold array:",hold_Array);
 
- console.log(i + " index value is " + hold_Array[r]);
-    //  hold_Array.map((item, index, arr) => {
-    //   console.log (item, index, arr);
-    //  });
+ // console.log(i + " index value is " + hold_Array[r]);
+
  }
 console.log(random_Set);
 return random_Set;
 }
 
 
+// This checks to see if the last 2 cards clicked match.
+function checkForMatch(){
+  let letter1 = card_One.slice(-1);
+  let letter2 = card_Two.slice(-1);
+  if ( letter1 === letter2){
+    console.log("Match Made!");
+  }
+}
+
+
+function returnFaceDown(){
+
+// Creates an array of all the class of all the show tiles.
+let x = document.getElementsByClassName("show");
+console.log("There are " + x.length + "show tiles.");
+// Hides all the show tiles.
+for( let i = 0; i < x.length; i++ ){
+ x[i].setAttribute("class", "hide");
+ }
+
+//
+// let x = document.getElementById('card_One');
+// x.setAttribute("class", "hide");
+// let y = document.getElementById('card_Two');
+// y.setAttribute("class", "hide");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Keeps track of the number of tiles flipped and card letters.
+let tiles_Flipped_Even = 0;
+let card_One;
+let card_Two;
 
 // This controls the actions of the cards when flipped.
 function clickToFlip() {
-console.log("Clicked a tile");
-console.log(this.id);
+// This checks to see if 2 cards are shown.
+tiles_Flipped_Even += 1;
+// console.log("Clicked a tile " + tiles_Flipped + " times.");
+   switch(tiles_Flipped_Even){
+     case 1: card_One = this.id;
+     break;
+     case 2: card_Two = this.id;
+             checkForMatch();
+     break;
+     case 3:
+     returnFaceDown();
+     tiles_Flipped_Even = 1;
+     card_One = this.id;
+    //  break;
+     default:
+    //  Insert code here to return 2 cards to hide.
+    // Probably a function.
+   }
+   console.log("Tiles e/o: " + tiles_Flipped_Even);
+console.log("card_One is " + card_One);
+  console.log("card_Two is " + card_Two);
+
+
+
+
+
+
+// console.log(this.id);
 // This makes the card change class and flip.
 this.setAttribute("class", "show");
 // This gives the letter on the tile.
 let letterOnCard = this.id.slice(-1);
-console.log("The letter on the card is " +letterOnCard);
+// console.log("The letter on the card is " +letterOnCard);
 
 // This gives the tile number as a string
 let num1 = this.id.charAt(4);
 let numBoth = this.id.charAt(4) + this.id.charAt(5);
 let unknown = this.id.charAt(5);
+let trueNum = 0;
 
      if (num1 === '1' && unknown >= 0){
-          console.log("The card number is " + numBoth);
+          // console.log("The card number is " + numBoth);
+          trueNum = numBoth;
      }
      else{
-       console.log("The card number is " + num1);
+      //  console.log("The card number is " + num1);
+      trueNum = num1;
      }
+     console.log("The card number is " + trueNum);
+// This ends the number code.
+
+
 }
 
 
